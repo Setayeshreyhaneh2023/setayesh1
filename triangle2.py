0ee5ebc1cb06287c9cbc51209360fd8ba785d2e1
Setayeshreyhaneh2023 <148361479+Setayeshreyhaneh2023@users.noreply.github.com>
2023-10-20 19:11:42 +0330

    Add files via upload

---
+++ triangle2.py
@@ -0,0 +1,8 @@
+a = int (input("Enter a: "))
+b = int (input("Enter b: "))
+c = int (input("Enter c: "))
+
+if a+b>c and b+c>a and a+c>b:
+    print("it is triangle")
+else:
+    print("run again")      
